e1d81554f3481467a64dbee27cfc51e69a72d123
wade <wade_peng@Hipposcreen-nc.com>
2026-01-20 10:49:38 +0800

    Build docs for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite' // 引入插件
 
 export default defineConfig({
   base: '/SMART-EEG/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     react(),
     tailwindcss(), // 啟用 Tailwind
